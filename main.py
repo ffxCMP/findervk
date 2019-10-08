a3d12235ca3caeb5b2273e9660f4bfc431c16aae
ffxCMP <56308099+ffxCMP@users.noreply.github.com>
2019-10-08 18:54:32 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,125 @@
+#import numpy
+#import scipy
+#import time
+import threading
+#import asyncio
+from image_match.goldberg import ImageSignature
+import requests
+#from settings import *
+from elasticsearch import Elasticsearch
+from image_match.elasticsearch_driver import SignatureES
+settings = open('settings.txt').read().split("\n")
+owner_id = settings[0]
+access_token = settings[1]
+url = settings[2]
+results1 = open('urls.txt', 'w')
+off = 0
+link_list = []
+count = []
+s = 0 #start
+e = 200 #end
+gis = ImageSignature()
+a = gis.generate_signature(url)
+
+
+def th_f(start,end):
+    for i in range(start,end):
+        try:
+            n = link_list[i]
+            b = gis.generate_signature(n)
+            raznica = gis.normalized_distance(a, b)
+            if raznica <0.4:
+                #print(f'Возможно совпадение - {link_list[i]}')
+                print('Возможно совпадение. Проверьте файл urls.txt')
+                print('Добавил ссылку в urls.txt')
+                with open('urls.txt', 'a') as results:
+                    results.write(link_list[i])
+                    results.close()
+                with open('urls.txt', 'a') as results:
+                    results.write('\n')
+                    results.close()
+        except:
+            continue
+
+
+
+def get_count():
+    global off, count
+    url ='https://api.vk.com/method/wall.get?'
+    params = {'owner_id' : owner_id,
+              'access_token': access_token,
+              'offset' : '0',
+              'v':'5.101',
+              'count':'100'}
+    r = requests.post(url,params=params)
+    base = r.json()
+    count = base['response']['count']
+    return count
+
+def get_posts(offset):
+    global off, count
+    url ='https://api.vk.com/method/wall.get?'
+    params = {'owner_id' : owner_id,
+              'access_token': access_token,
+              'offset' : offset,
+              'v':'5.101',
+              'count':'100'}
+    r = requests.post(url,params=params)
+    base = r.json()
+    count = base['response']['count']
+
+    for i in range(0,count):
+        try:
+            a = base['response']['items'][i]['attachments']
+          #  print('------')
+           # print('Атачмент')
+           # print(a)
+           # print('------')
+           # print('\n')
+            att_count = len(base['response']['items'][i]['attachments'])
+            for j in range(0,att_count):
+                try:
+                    b = base['response']['items'][i]['attachments'][j]['photo']['sizes'][0]['url']
+                   # print('------')
+                    #print('Sizes')
+                   # print(b)
+                    #print('------')
+                    #print('\n')
+                    #sizes_count = len(b)
+                    link_list.append(b)
+                except:
+                    continue
+        except:
+            continue
+    off += 100
+
+if __name__ == "__main__":
+    threads = list()
+    dlina = get_count()
+    dlina = dlina//100
+    print('Собираю все посты из группы...')
+    for i in range(0,dlina):
+        get_posts(off)
+
+    print('Ищу одинаковые или похожие картинки...')
+
+    for index in range(len(link_list) // 200):
+        threading.Thread(target=th_f, args=(s, e)).start()
+        s += 200
+        e += 200
+
+''''
+    for index in range(len(link_list)//200):
+        x = threading.Thread(target=th_f, args=(s,e))
+        threads.append(x)
+        x.start()
+        s+=200
+        e+=200
+
+    for i in range(0,len(link_list)-1):
+        n = link_list[i]
+        b = gis.generate_signature(n)
+        raznica = gis.normalized_distance(a, b)
+        if raznica <0.4:
+            print(f'Возможно совпадение - {link_list[i]}')
+'''
